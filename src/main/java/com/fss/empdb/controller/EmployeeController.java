865ca154fe210d450518d36485119c04262125cb
kirti765 <kirti76510@gmail.com>
2020-04-10 12:51:10 +0530

    Request mapping

--- src/main/java/com/fss/empdb/controller/EmployeeController.java
+++ src/main/java/com/fss/empdb/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/services")
 public class EmployeeController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
--- src/main/java/com/fss/empdb/controller/MasterController.java
+++ src/main/java/com/fss/empdb/controller/MasterController.java
@@ -6,71 +6,69 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/master")
 public class MasterController {
 
     @Autowired
     MasterService masterService;
 
-    @GetMapping("/master/get-all-department")
+    @GetMapping("/get-all-department")
     public List<Department> getAllDepartment() {
         return masterService.getAllDepartment();
     }
 
-    @GetMapping("/master/get-all-designation")
+    @GetMapping("/get-all-designation")
     public List<Designation> getAllDesignation() {
         return masterService.getAllDesignation();
     }
 
-    @GetMapping("/master/get-all-account")
+    @GetMapping("/get-all-account")
     public List<Account> getAllAccount() {
         return masterService.getAllAccount();
     }
 
-    @GetMapping("/master/get-all-region")
+    @GetMapping("/get-all-region")
     public List<Region> getAllRegion() {
         return masterService.getAllRegion();
     }
 
-    @GetMapping("/master/get-all-location")
+    @GetMapping("/get-all-location")
     public List<Location> getAllLocation() {
         return masterService.getAllLocation();
     }
 
-    @GetMapping("/master/get-all-skill")
+    @GetMapping("/get-all-skill")
     public List<Skill> getAllSkill() {
         return masterService.getAllSkill();
     }
 
-    @GetMapping("/master/get-all-service-line")
+    @GetMapping("/get-all-service-line")
     public List<ServiceLine> getAllServiceLine() {
         return masterService.getAllServiceLine();
     }
 
-    @GetMapping("/master/get-all-billable-status")
+    @GetMapping("/get-all-billable-status")
     public List<BillableStatus> getAllBillableStatus() {
         return masterService.getAllBillableStatus();
     }
 
-    @GetMapping("/master/get-all-grade")
+    @GetMapping("/get-all-grade")
     public List<Grade> getAllGrade() {
         return masterService.getAllGrade();
     }
 
-    @GetMapping("/master/get-all-academics")
+    @GetMapping("/get-all-academics")
     public List<Academics> getAllAcademics() {
         return masterService.getAllAcademics();
     }
 
-    @GetMapping("/master/get-all-project-tagging")
+    @GetMapping("/get-all-project-tagging")
     public List<ProjectTagging> getAllProjectTagging() {
         return masterService.getAllProjectTagging();
     }
