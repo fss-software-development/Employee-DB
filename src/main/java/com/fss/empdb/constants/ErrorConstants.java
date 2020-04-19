3a46a397563772f2008e1464026c91877b5d045c
kirti765 <kirti76510@gmail.com>
2020-04-20 00:13:37 +0530

    variable added

--- src/main/java/com/fss/empdb/constants/ErrorConstants.java
+++ src/main/java/com/fss/empdb/constants/ErrorConstants.java
@@ -7,4 +7,6 @@ public class ErrorConstants {
     public static final String EMPLOYEE_NOT_FOUND= "Employee not found for this id ::";
 
     public static final String CUSTOMER_NOT_FOUND= "Customer not found for this id ::";
+
+    public static final String DATA_NOT_FOUND= "Data not found for this id ::";
 }
