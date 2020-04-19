d4c341c948035d0931ea49e55c2bf5f298dfd4be
kirti765 <kirti76510@gmail.com>
2020-04-20 00:02:48 +0530

    ProjectRepository Tagging

--- src/main/java/com/fss/empdb/repository/ProjectRepository.java
+++ src/main/java/com/fss/empdb/repository/ProjectRepository.java
@@ -1,9 +1,12 @@
 package com.fss.empdb.repository;
 
 import com.fss.empdb.domain.Project;
-import com.fss.empdb.domain.ProjectTagging;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ProjectRepository extends JpaRepository<Project,Long>, JpaSpecificationExecutor<Project> {
+
+    public Project findByProjectName(String projectName);
+
+   // public List<Project> findByProjectName(String projectName);
 }
