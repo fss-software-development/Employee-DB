33892f3744f60a03040bc9b311767792dd9d47ff
kirti765 <kirti76510@gmail.com>
2020-04-19 23:59:23 +0530

    ProjectController added with get all

--- src/main/java/com/fss/empdb/controller/ProjectController.java
+++ src/main/java/com/fss/empdb/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.fss.empdb.controller;
 
 
+import com.fss.empdb.domain.Employee;
 import com.fss.empdb.domain.Project;
 import com.fss.empdb.domain.SearchCriteria;
 import com.fss.empdb.service.ProjectService;
@@ -31,9 +32,34 @@ public class ProjectController {
     }
 
     //Get Project Details By Id - View Case
-    @GetMapping("/get-all-project/{id}")
-    public ResponseEntity<Project> getProjectById(@PathVariable(value = "id") Long employeeId) {
-        return ResponseEntity.ok().body(projectService.getProjectById(employeeId));
+    @GetMapping("/get-projectById/{id}")
+    public ResponseEntity<Project> getProjectById(@PathVariable(value = "id") Long projectId) {
+        return ResponseEntity.ok().body(projectService.getProjectById(projectId));
+    }
+
+
+    //Get Project Details By Id - View Case
+    @GetMapping("/get-projectByName/{name}")
+    public ResponseEntity<Project> getProjectByName(@PathVariable(value = "name") String projectName) {
+        LOGGER.info("-------projectName---------" + projectName);
+        return ResponseEntity.ok().body(projectService.getProjectByName(projectName));
+    }
+
+    //Add & Update Employee
+    @PostMapping(value = "/project-add-update")
+    public ResponseEntity<Project> createOrUpdateProject(@RequestBody Project project)  {
+        LOGGER.info("-------Project---------" + project);
+
+        Project proj = projectService.createOrUpdateProject(project);
+        return new ResponseEntity<Project>(proj, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/project-delete/{id}")
+    public ResponseEntity<?> deleteProject(@PathVariable(value = "id") Long projectId){
+        LOGGER.info("-------Project---------" + projectId);
+
+        projectService.deleteProject(projectId);
+        return new ResponseEntity<Project>(new HttpHeaders(), HttpStatus.OK);
     }
 
 }
