e5ef94125c37ff7eebe54b5d8796102df40d63e1
09rakeshmhaske <rakeshm@fss.co.in>
2020-04-20 15:15:15 +0530

    Master end points

--- src/main/java/com/fss/empdb/controller/AccountController.java
+++ src/main/java/com/fss/empdb/controller/AccountController.java
@@ -1,21 +1,14 @@
 package com.fss.empdb.controller;
 
 import com.fss.empdb.domain.Account;
-import com.fss.empdb.domain.Employee;
 import com.fss.empdb.service.AccountService;
-import com.fss.empdb.service.CustomerService;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
