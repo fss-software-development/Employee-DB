b5d552027cfb22a2c97c2a6050a4fd4f5cf64549
09rakeshmhaske <rakeshm@fss.co.in>
2020-04-22 16:23:26 +0530

    Test Cases

---
+++ src/main/resources/testdb/tear-down-masters-db.sql
@@ -0,0 +1,18 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE employee_db.academics;
+TRUNCATE employee_db.billable_status;
+TRUNCATE employee_db.department;
+TRUNCATE employee_db.designation;
+TRUNCATE employee_db.grade;
+TRUNCATE employee_db.location;
+TRUNCATE employee_db.project_tagging;
+TRUNCATE employee_db.region;
+TRUNCATE employee_db.role;
+TRUNCATE employee_db.service_line;
+TRUNCATE employee_db.skill;
+TRUNCATE employee_db.tools;
+TRUNCATE employee_db.user;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
