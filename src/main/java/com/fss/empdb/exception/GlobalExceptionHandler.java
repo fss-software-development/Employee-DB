95c9aa1049293848080944bd09d9b9c76a9e72b8
kirti765 <kirti76510@gmail.com>
2020-04-29 19:03:00 +0530

    Exception Handling

--- src/main/java/com/fss/empdb/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fss/empdb/exception/GlobalExceptionHandler.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.http.*;
 
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 import java.util.Date;
 
 @ControllerAdvice
@@ -21,4 +24,62 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<Object> handleAnyException(WebRequest request, NullPointerException ex) {
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription==null)
+            errorMessageDescription=ex.toString();
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "NullPointerException", request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<Object> handleArithmeticExceptionError(WebRequest request, ArithmeticException ex) {
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription==null)
+            errorMessageDescription=ex.toString();
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "ArithmeticException", request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+    public ResponseEntity<Object> handleArrayIndexOutOfBoundsExceptionError(WebRequest request, ArrayIndexOutOfBoundsException ex) {
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription==null)
+            errorMessageDescription=ex.toString();
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "ArrayIndexOutOfBoundsException", request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<Object> handleSQLExceptionError(WebRequest request, FileNotFoundException ex) {
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription==null)
+            errorMessageDescription=ex.toString();
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "FileNotFoundException", request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<Object> handleSQLExceptionError(WebRequest request, SQLException ex) {
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription==null)
+            errorMessageDescription=ex.toString();
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "SQLException", request.getDescription(false));
+
+
+        return new ResponseEntity<>(errorDetails, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
