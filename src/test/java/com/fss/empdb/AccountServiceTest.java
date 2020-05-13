64a6f2d45927083c8d8e97b1613bf8e8fb90c91a
09rakeshmhaske <rakeshm@fss.co.in>
2020-05-13 15:13:10 +0530

    added user_role_permission_id

--- src/test/java/com/fss/empdb/AccountServiceTest.java
+++ src/test/java/com/fss/empdb/AccountServiceTest.java
@@ -22,70 +22,70 @@
 //public class AccountServiceTest {
 //
 //
-//    @Autowired
-//    AccountService accountService;
-//
-//
-//    private Account newAccount() {
-//        Account acc = new Account();
-//        acc.setAccountName("RBI Bank3");
-//        Region reg = new Region();
-//        reg.setRegionId(3L);
-//        acc.setRegion(reg);
-//        return acc;
-//    }
-//
-//    private Account updateAccount() {
-//        Account acc = new Account();
-//        acc.setAccountId(7L);
-//        acc.setAccountName("Yes Bank3");
-//        Region reg = new Region();
-//        reg.setRegionId(5L);
-//        acc.setRegion(reg);
-//        return acc;
-//    }
-//
-//    private AccountSearchCriteria searchAccount() {
-//        AccountSearchCriteria acc = new AccountSearchCriteria();
-//        acc.setAccountName("");
-//        Region reg = new Region();
-//        reg.setRegionId(1L);
-//        Region reg1 = new Region();
-//        reg1.setRegionId(5L);
-//        Region[] regArr = {reg, reg1};
-//
-//        acc.setRegion(regArr);
-//        return acc;
-//    }
-//
-//    @Test
-//    public void createAccount_success() {
-//        Account acc = newAccount();
-//        Account newAcc = accountService.createAccount(acc);
-//        assertNotNull(newAcc);
-//        assertNotNull(newAcc.getAccountId());
-//    }
-//
-//    @Test
-//    public void updateAccount_success() {
-//        Account acc = updateAccount();
-//        Account newAcc = accountService.updateAccount(acc);
-//        assertNotNull(newAcc);
-//        assertNotNull(newAcc.getAccountId());
-//    }
-//
-//    @Test
-//    public void deleteAccount_success() {
-//        accountService.deleteAccount(5L);
-//    }
-//
-//    @Test
-//    public void searchAccount_success() {
-//        AccountSearchCriteria acc = searchAccount();
-//        System.out.println("Test Case " + acc);
-//        List<Account> newAcc = accountService.accountBySearch(acc);
-//        assertNotNull(newAcc);
-//    }
+////    @Autowired
+////    AccountService accountService;
+////
+////
+////    private Account newAccount() {
+////        Account acc = new Account();
+////        acc.setAccountName("RBI Bank3");
+////        Region reg = new Region();
+////        reg.setRegionId(3L);
+////        acc.setRegion(reg);
+////        return acc;
+////    }
+////
+////    private Account updateAccount() {
+////        Account acc = new Account();
+////        acc.setAccountId(7L);
+////        acc.setAccountName("Yes Bank3");
+////        Region reg = new Region();
+////        reg.setRegionId(5L);
+////        acc.setRegion(reg);
+////        return acc;
+////    }
+////
+////    private AccountSearchCriteria searchAccount() {
+////        AccountSearchCriteria acc = new AccountSearchCriteria();
+////        acc.setAccountName("");
+////        Region reg = new Region();
+////        reg.setRegionId(1L);
+////        Region reg1 = new Region();
+////        reg1.setRegionId(5L);
+////        Region[] regArr = {reg, reg1};
+////
+////        acc.setRegion(regArr);
+////        return acc;
+////    }
+////
+////    @Test
+////    public void createAccount_success() {
+////        Account acc = newAccount();
+////        Account newAcc = accountService.createAccount(acc);
+////        assertNotNull(newAcc);
+////        assertNotNull(newAcc.getAccountId());
+////    }
+////
+////    @Test
+////    public void updateAccount_success() {
+////        Account acc = updateAccount();
+////        Account newAcc = accountService.updateAccount(acc);
+////        assertNotNull(newAcc);
+////        assertNotNull(newAcc.getAccountId());
+////    }
+////
+////    @Test
+////    public void deleteAccount_success() {
+////        accountService.deleteAccount(5L);
+////    }
+////
+////    @Test
+////    public void searchAccount_success() {
+////        AccountSearchCriteria acc = searchAccount();
+////        System.out.println("Test Case " + acc);
+////        List<Account> newAcc = accountService.accountBySearch(acc);
+////        assertNotNull(newAcc);
+////    }
 //
 //
 //}
