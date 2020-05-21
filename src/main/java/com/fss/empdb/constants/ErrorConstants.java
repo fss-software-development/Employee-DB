d6738f6b6468b487715d9e6c07cbc17120d6b8d5
kirti765 <kirti76510@gmail.com>
2020-05-21 20:13:00 +0530

    Is reset required

--- src/main/java/com/fss/empdb/constants/ErrorConstants.java
+++ src/main/java/com/fss/empdb/constants/ErrorConstants.java
@@ -6,9 +6,11 @@ public class ErrorConstants {
 
     public static final String EMPLOYEE_NOT_FOUND= "Employee not found for this id ::";
     public static final String CUSTOMER_NOT_FOUND= "Customer not found for this id ::";
+
     public static final String DATA_NOT_FOUND= "Data not found for this id ::";
     public static final String FORGOT_PWD="Password Reset Successfully ";
     public  static  final String IS_RESET_REQUIRED="Y";
+
     private static final String SUCCESS_STATUS = "success";
     private static final String ERROR_STATUS = "error";
     private static final int CODE_SUCCESS = 100;
